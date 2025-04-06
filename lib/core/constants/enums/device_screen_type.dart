d5a5d4eadd4ea32728fa90660e3cbbdeb366074f
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-06 21:22:59 +0330

    Add packages

---
+++ lib/core/constants/enums/device_screen_type.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+enum DeviceScreenType { mobile, tablet, desktop }
+
+DeviceScreenType getDeviceType(BoxConstraints constraints) {
+  final width = constraints.maxWidth;
+  if (width >= 1000) {
+    return DeviceScreenType.desktop;
+  } else if (width >= 600) {
+    return DeviceScreenType.tablet;
+  } else {
+    return DeviceScreenType.mobile;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:accounting/core/shared/global_config.dart';
 import 'package:accounting/core/presentation/app_widget.dart';
 
