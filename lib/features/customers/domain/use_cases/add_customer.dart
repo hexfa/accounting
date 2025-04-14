ffd47d26d1ebd4cf65fca18ae510d621346e921b
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-14 19:32:31 +0330

    Make customer repo

--- lib/features/customers/domain/use_cases/add_customer.dart
+++ lib/features/customers/domain/use_cases/add_customer.dart
@@ -1,17 +1,19 @@
+
+
+import 'package:accounting/core/errors/failures.dart';
+import 'package:accounting/core/use_cases/base_use_case.dart';
+import 'package:accounting/features/customers/domain/entities/customer.dart';
+import 'package:accounting/features/customers/domain/repositories/customer_repository.dart';
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
-import '../../../../core/errors/failures.dart';
-import '../../../../core/use_cases/base_use_case.dart';
-import '../entities/customer.dart';
-import '../repositories/customer_repository.dart';
 
 @lazySingleton
 class AddCustomer extends BaseUseCase<void, Customer> {
   final CustomerRepository repository;
-
   AddCustomer(this.repository);
 
   @override
   Future<Either<Failure, void>> execute(Customer params) async {
     return repository.addCustomer(params);
+  }
 }
