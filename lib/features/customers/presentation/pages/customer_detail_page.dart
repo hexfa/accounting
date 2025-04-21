4bd8afaf4cff8bf2002b9c4ea191ec8be9c6aa38
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-21 09:47:41 +0330

    Make customer detail page.

--- lib/features/customers/presentation/pages/customer_detail_page.dart
+++ lib/features/customers/presentation/pages/customer_detail_page.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import '../../domain/entities/customer.dart';
-
-
 class CustomerDetailPage extends StatelessWidget {
   final String customerId;
 
