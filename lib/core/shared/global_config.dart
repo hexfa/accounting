d79b7d2b7e0f6e25d5852150c314b180c6b904a1
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-23 17:45:11 +0330

    Delete order from customer.

--- lib/core/shared/global_config.dart
+++ lib/core/shared/global_config.dart
@@ -11,6 +11,8 @@ import '../constants/app_strings.dart';
 import '../data/storage_services/shared_prefs_storage_service.dart';
 import '../di/injection.dart';
 class GlobalConfig {
+
+
   static late SharedPrefsStorageService storageService;
   static final secureStorage = getIt<SecureStorage>();
   static Future<void> initConfig() async {
