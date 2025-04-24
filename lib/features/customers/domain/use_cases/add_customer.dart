7f90b09f84e54551c1960b323f0e5332c4ce0746
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-24 16:18:16 +0330

    cleaning code

--- lib/features/customers/domain/use_cases/add_customer.dart
+++ lib/features/customers/domain/use_cases/add_customer.dart
@@ -6,7 +6,6 @@ import 'package:accounting/features/customers/domain/entities/customer.dart';
 import 'package:accounting/features/customers/domain/repositories/customer_repository.dart';
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
-
 @lazySingleton
 class AddCustomer extends BaseUseCase<void, Customer> {
   final CustomerRepository repository;
