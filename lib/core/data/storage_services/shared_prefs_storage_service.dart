c4dee0ab251ebf3753afe91aa28c2be5f177dd62
@hexfa <amir.dehdarian2009@gmail.com>
2025-04-29 22:33:10 +0330

    make errors and orders page

--- lib/core/data/storage_services/shared_prefs_storage_service.dart
+++ lib/core/data/storage_services/shared_prefs_storage_service.dart
@@ -17,6 +17,7 @@ class SharedPrefsStorageService {
   String getStringValue(String key) {
     return _prefs.getString(key) ?? "";
   }
+
   // get String list value
   List<String> getStringListValue(String key) {
     return _prefs.getStringList(key) ?? [];
