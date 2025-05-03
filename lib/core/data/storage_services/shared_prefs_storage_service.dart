d65f2987f87f4e7578b755b18740927faaf36af2
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-03 21:37:19 +0330

    make errors and orders page

--- lib/core/data/storage_services/shared_prefs_storage_service.dart
+++ lib/core/data/storage_services/shared_prefs_storage_service.dart
@@ -33,7 +33,6 @@ class SharedPrefsStorageService {
   Future<void> setStringList(String key, List<String> value) async {
     await _prefs.setStringList(key, value);
   }
-
   // remove key
   Future<void> removeKey(String key) async {
     await _prefs.remove(key);
