deb4c1b0d254c910f64dd6d9848d18f063030637
@hexfa <amir.dehdarian2009@gmail.com>
2025-05-11 15:57:06 +0330

    clean code

--- lib/features/application/presentation/widgets/single_splash_page.dart
+++ lib/features/application/presentation/widgets/single_splash_page.dart
@@ -11,6 +11,7 @@ Widget singleSplashPage({
   required SplashItem splashItem,
 }) {
 
+
   return Column(
       mainAxisAlignment: kIsWeb? MainAxisAlignment.start: MainAxisAlignment.center,
       crossAxisAlignment:kIsWeb? CrossAxisAlignment.center: CrossAxisAlignment.stretch,
