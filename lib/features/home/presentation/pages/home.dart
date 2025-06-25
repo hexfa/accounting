94ae0ac7760c4707e1d8fc112c76aa05c8e5b89f
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-25 11:37:04 +0330

    clean code

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -18,6 +18,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
 
 
 
+
   final List<Widget> _pages = const [
     ProductsPage(),
     Center(child: Text('سفارشات')),
