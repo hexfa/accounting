c5dc9e3475de09f1c9d622ca66d383bac16af129
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-29 09:55:25 +0330

    clean code

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -29,6 +29,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     _tabController = TabController(length: _pages.length, vsync: this);
   }
 
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
