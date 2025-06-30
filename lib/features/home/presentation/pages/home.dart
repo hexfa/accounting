63b1858684679d20b83c9f12dccde2a73aaf22cc
@hexfa <amir.dehdarian2009@gmail.com>
2025-06-30 11:46:11 +0330

    clean code

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -44,7 +44,6 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
 
 
 
-
         if (deviceType == DeviceScreenType.desktop) {
           return Scaffold(
             body: Row(
