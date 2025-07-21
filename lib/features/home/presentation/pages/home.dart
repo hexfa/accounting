a031dcf874c010b33bb620ae31c77393b705ef5b
@hexfa <amir.dehdarian2009@gmail.com>
2025-07-21 13:00:04 +0330

    clean code

--- lib/features/home/presentation/pages/home.dart
+++ lib/features/home/presentation/pages/home.dart
@@ -43,7 +43,6 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
                     child: AppMenu(),
                   ),
                 ),
-
                 Expanded(
                   child: Column(
                     children: [
